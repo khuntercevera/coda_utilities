9e44f071c5068cf65881603ac87684511fc444c5
Kristen Hunter-Cevera <kristen@lunamoth.localdomain>
2020-10-16 17:45:34 -0400

    plots of approximate critical Wilk's lamdba

--- multivariate_regression/ilr_multivariate_regression.jl
+++ multivariate_regression/ilr_multivariate_regression.jl
@@ -82,7 +82,7 @@ Y_fa = ilr_y[fa,:]
 
 
 #record in a dataframe
-stepwise_df=DataFrame(season=String[], step=Int64[], base=String[], var=String[], Lambda=Float64[], F_pval=Float64[], B_hat=Array{Union{Missing, Float64},2}[])
+stepwise_df=DataFrame(season=String[], step=Int64[], base=String[], var=String[], Lambda=Float64[], F=Float64[], df1=Float64[], df2=Float64[], F_pval=Float64[], B_hat=Array{Union{Missing, Float64},2}[])
 
 # first, find out which variable results in lowest lambda value:
 seasons=["winter/spring", "summer", "fall"]
@@ -126,7 +126,7 @@ for j=1:3
         f, df1 ,df2=approx_F(Λf,p,vE,vH)
         pvalF=1-cdf(FDist(df1,df2),f)
 
-        push!(stepwise_df,[season,0,string(vars[q]), "-", Λf, pvalF, B_hat])
+        push!(stepwise_df,[season,0,string(vars[q]), "-", Λf, f, df1, df2, pvalF, B_hat])
 
 end
 
@@ -134,12 +134,12 @@ end
 r=findall(stepwise_df[!,:season] .== season)
 global qbase=argmin(stepwise_df[r,:Lambda])
 global qtest=setdiff(1:6,qbase)
-global step=1
+global stepp=1
 
-while step <= 6
+while stepp <= 6
     global qtest
     global qbase
-    global step
+    global stepp
 
     for q in qtest
 
@@ -159,7 +159,7 @@ while step <= 6
         Λ_test = Λf ./ Λr #Lamba to compare addition of extra variable
 
         # approximate F and χ statistics:
-        vH = length(q) #additional vars
+        vH = length(q) #additional varsglobal step=1
         vE = n-(length(qbase))-1 #n - x vars in reduced model -1
         p = size(Y,2) #num y vars
 
@@ -170,20 +170,20 @@ while step <= 6
         pvalF=1-cdf(FDist(df1,df2),f)
         #pvalchi = 1-cdf(Chisq(df),chi)
         if length(qbase) == 1
-            push!(stepwise_df,[season, step, string(vars[qbase]), string(vars[q]), Λ_test, pvalF, B_hat])
+            push!(stepwise_df,[season, stepp, string(vars[qbase]), string(vars[q]), Λ_test, f, df1, df2, pvalF, B_hat])
         else
-            push!(stepwise_df,[season, step, join(string.(vars[qbase]),"-"), string(vars[q]), Λ_test, pvalF, B_hat])
+            push!(stepwise_df,[season, stepp, join(string.(vars[qbase]),"-"), string(vars[q]), Λ_test,f, df1, df2, pvalF, B_hat])
         end
 
     end #end qtest loop
 
-    r=findall((stepwise_df[!,:step] .== step) .& (stepwise_df[!,:season] .== season))
+    r=findall((stepwise_df[!,:step] .== stepp) .& (stepwise_df[!,:season] .== season))
 
     if any(stepwise_df[r,:F_pval] .< 0.05) #alright- another variable does add significant explanation!
 
         qbase=vcat(qbase,qtest[argmin(stepwise_df[r,:Lambda])])
         qtest=setdiff(1:6,qbase)
-        step=step .+ 1
+        stepp=stepp .+ 1
 
     else
         break
@@ -277,6 +277,7 @@ yhat_simplex_temp = invilr(Y_hat_temp,bal)
 @save "/home/kristen/Documents/V6V8_analysis/analysis_products/oligo_ilr_regression_output2.jld2" stepwise_df y_hat yhat_simplex bhat_fall_simplex bhat_summer_simplex bhat_wisp_simplex
 
 ## if need to add to oligo_df_zero_plus dataframe:
+
 # oligo_df_plus.ilr1 = ilr_y[:,1]
 # oligo_df_plus.ilr2 = ilr_y[:,2]
 # oligo_df_plus.ilr3 = ilr_y[:,3]
@@ -286,25 +287,46 @@ yhat_simplex_temp = invilr(Y_hat_temp,bal)
 #oligo_df_plus.yearday = dayofyear.(oligo_df_plus[!,:date])
 #CSV.write("/home/kristen/Documents/V6V8_analysis/scripts/rel_abnd_analysis_scripts/matlab_scripts/olgio_df_plus_with_ilr.csv", oligo_df_plus, delim = ',')
 
-a=DataFrame(yhat_1=yhat_simplex[:,1],
-yhat_2=yhat_simplex[:,2],
-yhat_3=yhat_simplex[:,3],
-yhat_4=yhat_simplex[:,4],
-yhat_5=yhat_simplex[:,5],
-yhat_6=yhat_simplex[:,6],
-yhat_temp1=yhat_simplex_temp[:,1],
-yhat_temp2=yhat_simplex_temp[:,2],
-yhat_temp3=yhat_simplex_temp[:,3],
-yhat_temp4=yhat_simplex_temp[:,4],
-yhat_temp5=yhat_simplex_temp[:,5],
-yhat_temp6=yhat_simplex_temp[:,6],
-yhat_ilr1=Y_hat[:,1],
-yhat_ilr2=Y_hat[:,2],
-yhat_ilr3=Y_hat[:,3],
-yhat_ilr4=Y_hat[:,4],
-yhat_ilr5=Y_hat[:,5])
+b=convert(DataFrame,[X_su y[su[ii],:]]) #Y_fa
+bnames=names(b)
+for j=1:6
+    rename!(b,bnames[j] => vars[j])
+end
 
-CSV.write("/home/kristen/Documents/V6V8_analysis/scripts/rel_abnd_analysis_scripts/matlab_scripts/yhat_simplex.csv",a, delim = ',')
+# rename!(b,:x7 => :ilr1)
+# rename!(b,:x8 => :ilr2)
+# rename!(b,:x9 => :ilr3)
+# rename!(b,:x10 => :ilr4)
+# rename!(b,:x11 => :ilr5)
+
+rename!(b,:x7 => :O1)
+rename!(b,:x8 => :O2)
+rename!(b,:x9 => :O3)
+rename!(b,:x10 => :O4)
+rename!(b,:x11 => :O5)
+rename!(b,:x12 => :O6)
+
+CSV.write("/home/kristen/Documents/V6V8_analysis/scripts/R_scripts/su_data.csv", b, delim = ',')
+
+# a=DataFrame(yhat_1=yhat_simplex[:,1],
+# yhat_2=yhat_simplex[:,2],
+# yhat_3=yhat_simplex[:,3],
+# yhat_4=yhat_simplex[:,4],
+# yhat_5=yhat_simplex[:,5],
+# yhat_6=yhat_simplex[:,6],
+# yhat_temp1=yhat_simplex_temp[:,1],
+# yhat_temp2=yhat_simplex_temp[:,2],
+# yhat_temp3=yhat_simplex_temp[:,3],
+# yhat_temp4=yhat_simplex_temp[:,4],
+# yhat_temp5=yhat_simplex_temp[:,5],
+# yhat_temp6=yhat_simplex_temp[:,6],
+# yhat_ilr1=Y_hat[:,1],
+# yhat_ilr2=Y_hat[:,2],
+# yhat_ilr3=Y_hat[:,3],
+# yhat_ilr4=Y_hat[:,4],
+# yhat_ilr5=Y_hat[:,5])
+#
+# CSV.write("/home/kristen/Documents/V6V8_analysis/scripts/rel_abnd_analysis_scripts/matlab_scripts/yhat_simplex.csv",a, delim = ',')
 
 
 ## If want to just models individually:
@@ -375,3 +397,39 @@ corspearman(convert(Array{Float64},oligo_df_plus[ii,:PO4]),ilr_y[ii,4])
 
 plot(x=oligo_df_plus[ii,:PO4],y=ilr_y[ii,4], Geom.point)
 plot(x=oligo_df_plus[ii,:PO4],y=ilr_y[ii,5], Geom.point)
+
+## plots for Wilk's Lambda...
+
+#Sanity check just with interpolation....
+#for:
+#p=5 #number of response variables
+#h=1 #number of additional variables compared to reduced - 1 in our case if want to test significance of added variable
+
+#vE = n - q- 1 #number of samples, number of vars in full model -1
+
+#testing for light signficance:
+wispE = length(wisp) - 2 - 1
+suE = length(ii)- 3 -1 #had two significant vars 
+faE = length(fa)- 2 -2
+
+vE=[10:30; 40; 60; 80; 100]
+cval = [0.215; 0.261; 0.303; 0.341; 0.376; 0.407; 0.436; 0.462; 0.486; 0.508; 0.529; 0.548; 0.565; 0.581; 0.596; 0.610; 0.623; 0.635; 0.647; 0.658;0.668; 0.744; 0.825; 0.867; 0.893]
+
+l1 = layer(x=vE,y=cval,Geom.point)
+l2 = layer(x=vE,y=cval,Geom.line)
+l3= layer(x=[wispE], y=[stepwise_df[7,:Lambda]], Geom.point, style(default_color=RGB(0,0,0),point_size=2mm),shape=[Shape.square]) #,style(default_color=RGB(0,0,0), point_shapes=[square]))
+p1=plot(l1,l2,l3)
+
+
+l1 = layer(x=vE,y=cval,Geom.point)
+l2 = layer(x=vE,y=cval,Geom.line)
+l3= layer(x=[suE], y=[stepwise_df[27,:Lambda]], Geom.point, style(default_color=RGB(0,0,0),point_size=2mm),shape=[Shape.square]) #,style(default_color=RGB(0,0,0), point_shapes=[square]))
+p2=plot(l1,l2,l3)
+
+l1 = layer(x=vE,y=cval,Geom.point)
+l2 = layer(x=vE,y=cval,Geom.line)
+l3= layer(x=[faE], y=[stepwise_df[40,:Lambda]], Geom.point, style(default_color=RGB(0,0,0),point_size=2mm),shape=[Shape.square]) #,style(default_color=RGB(0,0,0), point_shapes=[square]))
+p3=plot(l1,l2,l3)
+
+Gadfly.set_default_plot_size(30cm,10cm)
+gridstack([p1 p2 p3])
